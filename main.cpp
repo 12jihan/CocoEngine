4e6983f943998552af1e5a58fa41c53c364c2b9b
12jikan <jareem.e.hoff@gmail.com>
2023-01-07 21:40:49 -0500

    first functioning window

--- main.cpp
+++ main.cpp
@@ -1,5 +1,37 @@
+#include <iostream>
+#include <GLFW/glfw3.h>
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+
+    GLFWwindow* window;
+    if( !glfwInit() )
+    {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        exit( EXIT_FAILURE );
+    }
+
+    window = glfwCreateWindow(300, 300, "CocoEngine", NULL, NULL);
+    if (!window)
+    {
+        fprintf( stderr, "Failed to open GLFW window\n" );
+        glfwTerminate();
+        exit( EXIT_FAILURE );
+    }
+
+    // Main loop:
+    while(!glfwWindowShouldClose(window)) 
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+
+    //Terminate GLFW window:
+    glfwTerminate();
+
+
+
+
     return 0;
 }
