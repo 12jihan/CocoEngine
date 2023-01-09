de3b2bd58dec22a5cd6450e6ca345544975b2e19
12jikan <jareem.e.hoff@gmail.com>
2023-01-08 22:37:49 -0500

    fixed issues

--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 int main(int argc, char const *argv[])
 {
-    std::cout << "asdasdasd" << std::endl;
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 800, "fucking window", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create window" << std::endl;
+        glfwTerminate();
+        return -1;
+    } 
+    glfwMakeContextCurrent(window);
+    gladLoadGL();
+
+    glViewport(0, 0, 800, 800);
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
